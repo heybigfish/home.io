ed88ad3deaaa8dde26b48968715eef4cf4fe2c8f
3y <335215709@qq.com>
2023-08-10 18:06:01 +0800

    fix: name

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="top-container">top</div>
+  <div class="top-container">
+    <div class="left">logo</div>
+    <div class="right">
+      <div class="item">首页</div>
+      <div class="item">笔记</div>
+      <div class="item">随记</div>
+      <div class="item">相册</div>
+      <div class="item">关于我</div>
+    </div>
+  </div>
 </template>
 
 <script setup lang='ts'>
-
 </script>
 
 <style lang="less" scoped>
@@ -15,5 +23,11 @@
   align-items: center;
   justify-content: space-between;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
+  .right {
+    display: flex;
+    .item + .item {
+      margin-left: 15px;
+    }
+  }
 }
 </style>
