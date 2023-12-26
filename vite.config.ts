9a137fde9b4fbd6a3610dcf24f438ac8958eed82
3y <335215709@qq.com>
2023-12-26 17:29:16 +0800

    fix: 格式校验

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,13 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       vue(),
-      visualizer(),
+      visualizer({
+        gzipSize: true,
+        brotliSize: true,
+        emitFile: false,
+        filename: 'test.html', //分析图生成的文件名
+        open: true //如果存在本地服务端口，将在打包后自动展示
+      }),
       AutoImport({
         resolvers: [ElementPlusResolver()]
       }),
